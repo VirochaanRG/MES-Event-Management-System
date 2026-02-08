c795eaddac514ffbfae6973b644ffe6172fd0245
Virochaan <39714123+VirochaanRG@users.noreply.github.com>
2026-02-08 15:52:02 -0500

    Prepare for prod

---
+++ src/db/src/db.js
@@ -0,0 +1,20 @@
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
+import * as schemas from './schemas';
+// Load environment variables from .env
+import { config } from "../../config/config";
+// Create a Postgres connection pool
+const pool = new Pool({
+    connectionString: config.DATABASE_URL,
+});
+pool.connect()
+    .then((client) => {
+    console.log('✅ Connected to PostgreSQL!');
+    client.release();
+})
+    .catch((err) => {
+    console.error('❌ Failed to connect to PostgreSQL:');
+    console.error(err);
+});
+// Initialize drizzle ORM
+export const db = drizzle(pool, { schema: schemas });
---
+++ src/db/src/schemas/form.js
@@ -0,0 +1,45 @@
+import { pgTable, serial, varchar, text, timestamp, integer, json, boolean } from "drizzle-orm/pg-core";
+export const form = pgTable("form", {
+    id: serial("id").primaryKey().notNull(),
+    name: varchar("name", { length: 255 }).notNull(),
+    description: text("description"),
+    createdAt: timestamp("created_at", { withTimezone: true }).defaultNow()
+});
+export const formSubmissions = pgTable("form_submissions", {
+    id: serial("id").primaryKey().notNull(),
+    userId: text("user_id")
+        .notNull(),
+    formId: integer("form_id")
+        .notNull()
+        .references(() => form.id, { onDelete: "cascade" }),
+    createdAt: timestamp("created_at", { withTimezone: true }).defaultNow(),
+    updatedAt: timestamp("updated_at", { withTimezone: true }).defaultNow(),
+});
+export const formQuestions = pgTable("form_questions", {
+    id: serial("id").primaryKey().notNull(),
+    formId: integer("form_id")
+        .notNull()
+        .references(() => form.id, { onDelete: "cascade" }),
+    questionType: varchar("question_type", { length: 100 }).notNull(),
+    questionTitle: text("question_title"),
+    optionsCategory: text("options_category"),
+    qorder: integer("qorder").notNull(),
+    createdAt: timestamp("created_at", { withTimezone: true }).defaultNow(),
+    parentQuestionId: integer("parent_question_id").references(() => formQuestions.id),
+    enablingAnswers: json("enabling_answers").$type(),
+    required: boolean("required").notNull().default(false)
+});
+export const formAnswers = pgTable("form_answers", {
+    id: serial("id").primaryKey().notNull(),
+    questionType: varchar("question_type", { length: 100 }).notNull(),
+    formId: integer("form_id")
+        .notNull()
+        .references(() => form.id, { onDelete: "cascade" }),
+    userId: text("user_id").notNull(),
+    questionId: integer("question_id")
+        .notNull()
+        .references(() => formQuestions.id, { onDelete: "cascade" }),
+    answer: text("answer"),
+    createdAt: timestamp("created_at", { withTimezone: true }).defaultNow(),
+    submissionId: integer("submission_id").references(() => formSubmissions.id, { onDelete: "cascade" })
+});
