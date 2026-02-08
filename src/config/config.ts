ceec6fdd54070041ec7cda171c6611dc4a448fa2
Virochaan <39714123+VirochaanRG@users.noreply.github.com>
2026-02-08 16:32:59 -0500

    Fixing for prod

--- src/config/config.ts
+++ src/config/config.ts
@@ -12,5 +12,6 @@ dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 export const config = {
   DATABASE_URL: process.env.DATABASE_URL || "",
   ENVIRONMENT: process.env.ENVIRONMENT || "dev",
+  VITE_API_URL: process.env.VITE_API_URL || "",
   // Add other environment variables here as needed
 } as const;
